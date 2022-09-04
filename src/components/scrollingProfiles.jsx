35c1fc91faa2c767030b918f50f62a0819d0822a
manikanta500 <manikanta@tharsha.com>
2022-09-04 20:28:39 +0530

    Added more images for scrollable profiles,corrected the code

--- src/components/scrollingProfiles.jsx
+++ src/components/scrollingProfiles.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { View,Image ,Text} from 'react-native'
+import { View, Image, Text } from 'react-native'
 
-function ScrollingProfiles({imgUrl,name}) {
-  return (
- <View style={{width:80,height:80,border:'3px solid #F20776',borderRadius:100,padding:5,margin:5,backgroundColor:'#FAFAFA'}}>
-       <img src={imgUrl} alt='Insta-Name' style={{width:'100%',height:'100%',borderRadius:100}}/>
+function ScrollingProfiles({ imgUrl, name }) {
+  return (<View style={{ flex: 1 }}>
+    <View style={{ width: 80, height: 80, border: '3px solid #F20776', padding: 5, margin: 5, backgroundColor: '#FAFAFA', borderRadius: 100 }}>
+      <img src={imgUrl} alt='profile' style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: 100 }} />
 
-       <Text style={{paddingVertical:10,textAlign:'center'}}>{name}</Text>
-       </View>
-      
-   
-  )
+
+    </View>
+    <Text style={{ textAlign: 'center' }}>{name}</Text>
+
+  </View>)
 }
 
 export default ScrollingProfiles
--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -5,13 +5,25 @@ import ScrollingProfiles from '../components/scrollingProfiles'
 
 const data = [
     {
-        imgUrl:require('../../assets/profile1.jpg'),name:'Pal'
+        imgUrl:require('../../assets/profile1.jpg'),name:'Beth'
     },
     {
         imgUrl:require('../../assets/profile2.jpg'),name:'Rocco'
     },
     {
-        imgUrl:require('../../assets/profile3.jpg'),name:'Sammy'
+        imgUrl:require('../../assets/profile3.jpg'),name:'James'
+    },
+    {
+        imgUrl:require('../../assets/profile4.jpg'),name:'Cathie'
+    },
+    {
+        imgUrl:require('../../assets/profile5.jpg'),name:'Sammy'
+    },
+    {
+        imgUrl:require('../../assets/profile6.jpg'),name:'Julian'
+    },
+    {
+        imgUrl:require('../../assets/profile7.jpg'),name:'Mike'
     }
 ];
 
@@ -23,18 +35,10 @@ horizontal={true}
 showsHorizontalScrollIndicator={false}>
     {data.map((sp,index)=> <ScrollingProfiles key={'scrollingProfile'+index} imgUrl={sp.imgUrl} name={sp.name}/>
     )}
- <ScrollingProfiles imgUrl={require('../../assets/profile1.jpg')} name='pal'/>
- <ScrollingProfiles imgUrl={require('../../assets/profile2.jpg')} name='pal'/>
- <ScrollingProfiles imgUrl={require('../../assets/profile3.jpg')} name='pal'/>
- <ScrollingProfiles imgUrl={require('../../assets/profile1.jpg')} name='pal'/> 
- <ScrollingProfiles imgUrl={require('../../assets/profile2.jpg')} name='pal'/>
- <ScrollingProfiles imgUrl={require('../../assets/profile3.jpg')} name='pal'/>
- <ScrollingProfiles imgUrl={require('../../assets/profile1.jpg')} name='pal'/>
+ 
  
 </ScrollView>
-  <View>
- 
-  </View>
+
   
   </>)
 }
